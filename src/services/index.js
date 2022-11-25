51dcab78e98afeaf1bc9eb5dab8e50a20ff9ed84
Aldana Guillermo <galdana@andreani.com>
2022-11-24 22:28:50 -0300

    Implementation services to request to api

---
+++ src/services/index.js
@@ -0,0 +1,7 @@
+export const login = ({ email, password }) =>
+  fetch('/login',
+    {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ email, password })
+    })
