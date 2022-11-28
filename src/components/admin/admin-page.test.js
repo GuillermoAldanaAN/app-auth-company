05c829d2b8b9e9e50afcb4a4bd28255e52a601cb
Aldana Guillermo <galdana@andreani.com>
2022-11-28 13:00:11 -0300

    Implementation of auth context in test

--- src/components/admin/admin-page.test.js
+++ src/components/admin/admin-page.test.js
@@ -2,9 +2,15 @@ import React from 'react'
 import { screen, render } from '@testing-library/react'
 import AdminPage from './admin-page'
 
+import { AuthContext } from '../../contexts/auth-context'
+
 describe('when login page is mount', () => {
-  it('must display the login title', () => {
-    render(<AdminPage/>)
-    expect(screen.getByText(/admin page/i)).toBeInTheDocument()
+  it('must display the login title and admin username', () => {
+    render(
+      <AuthContext.Provider value={{ user: { username: 'John Doe' } }}>
+        <AdminPage />
+      </AuthContext.Provider>
+    )
+    expect(screen.getByText(/john doe/i)).toBeInTheDocument()
   })
 })
